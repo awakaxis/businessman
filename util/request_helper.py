61b72a6851f2808fadc85ec601ad4da63604abb8
thedeadcupcake5 <flettzach@gmail.com>
2025-07-03 19:34:49 -0700

    Minor changes. Added the request_api function

--- util/request_helper.py
+++ util/request_helper.py
@@ -1,37 +1,33 @@
 import requests
-from util.log_helper import get_logger
+from log_helper import get_logger
 
 AUCTIONAPI = "https://api.hypixel.net/skyblock/auctions"
 
 LOGGER = get_logger(__name__)
 
 
-def get_auction_page_json(page_number):
+def request_api(page_number: int = 0):
     result = requests.get(url=AUCTIONAPI, params={"page": page_number})
     json_data = result.json()
 
-    if not result.status_code != 200:
-        LOGGER.warning("Unexpected status code: " + result.status_code)
+    if result.status_code != 200:
+        LOGGER.warning("Unexpected status code: " + str(result.status_code))
         return {}
     
     if not json_data["success"]:
         raise ValueError("API request failed.")
+    
+    return json_data
 
-    print(json_data)
+
+def get_auction_page_json(page_number: int):
+    json_data = request_api(page_number)
+
     return json_data
 
 
 def get_total_page_count():
-    result = requests.get(url=AUCTIONAPI)
-    json_data = result.json()
+    json_data = request_api()
 
-    if not result.status_code != 200:
-        LOGGER.warning("Unexpected status code: " + result.status_code)
-        return {}
-    
-    if not json_data["success"]:
-        raise ValueError("API request failed.")
-    
     return json_data["totalPages"]
 
-get_auction_page_json(5)
